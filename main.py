6ab64112c8037099da65f54b5969f22a79297c77
AngelMariscal01 <angel01diablin@gmail.com>
2024-05-15 19:33:10 -0600

    Commit

--- main.py
+++ main.py
@@ -3,11 +3,3 @@ import streamlit as st
 
 st.title("Introduccion a StreamLit")
 
-@st.cache_data
-def load_data(path: str):
-    data = pd.read_csv(path)
-    return data
-
-uploaded_file = st.file_uploader("Choose a file")
-df = load_data(uploaded_file)
-st.dataframe(df)
